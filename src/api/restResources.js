38bc47c33be5f05143d4099275b93661d32d373e
kha_ben <kha_ben@uni-bremen.de>
2021-03-18 14:14:01 +0100

    myCallBack Code

--- src/api/restResources.js
+++ src/api/restResources.js
@@ -4,12 +4,17 @@
 const axios = require("axios");
 
 function getAllData(){
-    return axios.get("https://demo.akademie.uni-bremen.de/rest/meta")
+    return axios.get("https://demo.akademie.uni-bremen.de/rest/meta?jsoncallback=acat")
                 .then(function (response){
-                    return ( response.data);
+                      return response.data.replace(');',' ').replace('(',' ');
+                      
+
+                     
+
                 })
 }
 
-getAllData().then(function (response){console.log(response);})
+
+getAllData().then(function (response){console.log(JSON.parse(response))})
 
  
--- src/taskpane/mycallback.js
+++ src/taskpane/mycallback.js
@@ -1,3 +1,16 @@
-let jsondata = function(testdata){
-    console.log(testData)
+"use strict"
+const axios = require("axios");
+
+function getAllData(){
+    return axios.get("https://demo.akademie.uni-bremen.de/rest/meta?jsonp=acat")
+                .then(function (response){
+                      return response.data;
+                      
+
+                     
+
+                })
 }
+
+
+getAllData().then(function (response){console.log(response)})
